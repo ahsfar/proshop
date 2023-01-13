f24b41747c93de075a1052b2b8c6bd2344757404
ahsfar <ahsfar21@gmail.com>
2023-01-13 15:43:25 +0800

    rating component and installed react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 
 import { Container } from "react-bootstrap"
+import {BrowserRouter as Router, Route } from "react-router-dom"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+
 
 const App = () => {
   return (
-    <>
+    <Router>
+    
     <Header/>
      <main>
        <Container>
-       <h1>Welcome to ProShop</h1>
+       <Route path="/" component={HomeScreen} exact/>
        </Container>
-       
      </main>
      <Footer/>
-    </>
+    
+    </Router>
   );
 }
 
