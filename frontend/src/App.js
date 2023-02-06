98812bfac855a8996db286ce081478c31c6c9f54
ahsfar <ahsfar21@gmail.com>
2023-02-06 18:26:41 +0800

    completed router 6 sec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 
 import { Container } from "react-bootstrap"
-import {BrowserRouter as Router, Route } from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
-
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
     <Router>
-    
     <Header/>
-     <main>
+     <main className="py-3"> 
        <Container>
-       <Route path="/" component={HomeScreen} exact/>
+       <Routes>
+       <Route path="/" element={<HomeScreen/>} exact/>
+       <Route path="/product/:id" element={<ProductScreen/>} />
+       </Routes>
        </Container>
      </main>
      <Footer/>
